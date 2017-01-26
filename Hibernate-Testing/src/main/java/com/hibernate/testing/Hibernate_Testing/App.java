74665d5a39ad6ce94f4670c5f313fbc3ee28aca9
amar <amar@192.168.0.104>
2017-01-26 23:43:51 +0530

    First cut of working commit

--- Hibernate-Testing/src/main/java/com/hibernate/testing/Hibernate_Testing/App.java
+++ Hibernate-Testing/src/main/java/com/hibernate/testing/Hibernate_Testing/App.java
@@ -1,4 +1,5 @@
 package com.hibernate.testing.Hibernate_Testing;
+
 import java.io.Serializable;
 
 import javax.persistence.Entity;
@@ -6,27 +7,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
-
-
 /**
  * Hello world!
  *
  */
-public class App implements Serializable 
-{
+public class App implements Serializable {
 	private int id;
-	   protected String fname; 
-	   protected String lname;
-	   protected String address;
-	   protected String phoneno;
+	protected String fname;
+	protected String lname;
+	protected String address;
+	protected String phoneno;
+
 	public int getId() {
 		return id;
 	}
-	public App(){}
+
+	public App() {
+	}
+
 	public App(String fname, String lname, String address, String phoneno) {
 		super();
-		
+
 		this.fname = fname;
 		this.lname = lname;
 		this.address = address;
@@ -36,29 +37,37 @@ public class App implements Serializable
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getFname() {
 		return fname;
 	}
+
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
+
 	public String getLname() {
 		return lname;
 	}
+
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getPhoneno() {
 		return phoneno;
 	}
+
 	public void setPhoneno(String phoneno) {
 		this.phoneno = phoneno;
 	}
-	   
+
 }
--- Hibernate-Testing/src/test/java/com/hibernate/testing/Hibernate_Testing/AppTest.java
+++ Hibernate-Testing/src/test/java/com/hibernate/testing/Hibernate_Testing/AppTest.java
@@ -1,89 +1,85 @@
-package com.hibernate.testing.Hibernate_Testing;import java.util.List; 
-import java.util.Date;
-import java.util.Iterator;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.hibernate.HibernateException; 
-import org.hibernate.Session; 
-import org.hibernate.Transaction;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+package com.hibernate.testing.Hibernate_Testing;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends App
-{
-    
-	private static SessionFactory factory; 
-	   public static void main(String[] args){
-		   
-		   try{
-		         factory = new Configuration().configure().buildSessionFactory();
-		      }catch (Throwable ex) { 
-		         System.err.println("Failed to create sessionFactory object." + ex);
-		         throw new ExceptionInInitializerError(ex); 
-		      }
-		      AppTest AT = new AppTest();
+public class AppTest {
 
-		      /* Add few employee records in database */
-		      Integer empID1= AT.addEmployee("Zara", "Ali", "bihar","jjigg");
-		     // Integer empID1 = AT.addemployee("Zara", "Ali", "bihar","jjigg");
-		    //  Integer empID2 = AT.addEmployee("Daisy", "Das", 5000);
-		    //  Integer empID3 = ME.addEmployee("John", "Paul", 10000);
+	private static SessionFactory factory;
 
-		      /* List down all the employees */
-		      AT.listEmployees();
+	public static void main(String[] args) {
 
-	   }
-    /**
-     * Rigourous Test :-)
-     */
-	private Integer addEmployee(String string, String string2, String string3, String string4) {
+		try {
+			Configuration configuration = new Configuration();
+			//configuration.addAnnotatedClass(com.hibernate.testing.Hibernate_Testing.App.class);
+			factory = configuration.configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		AppTest AT = new AppTest();
+
+		/* Add few employee records in database */
+		Integer empID1 = AT.addEmployee("Amar ", "singh", "bihar", "jjigg");
+		// AT.listEmployees();
+
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	private Integer addEmployee(String fname, String lname, String address, String phoneno) {
 		// TODO Auto-generated method stub
 		Session session = factory.openSession();
-	      Transaction tx = null;
-	      Integer employeeID = null;
-	      try{
-	         tx = session.beginTransaction();
-	         App employee = new App(fname, lname, address, phoneno);
-	         employeeID = (Integer) session.save(employee); 
-	         tx.commit();
-	      }catch (HibernateException e) {
-	         if (tx!=null) tx.rollback();
-	         e.printStackTrace(); 
-	      }finally {
-	         session.close(); 
-	      }
-	      return employeeID;
+		Transaction tx = null;
+		Integer employeeID = null;
+		try {
+			tx = session.beginTransaction();
+			App employee = new App(fname, lname, address, phoneno);
+			employeeID = (Integer) session.save(employee);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return employeeID;
 
-		
 	}
-	/* Method to  READ all the employees */
-	   public void listEmployees( ){
-	      Session session = factory.openSession();
-	      Transaction tx = null;
-	      try{
-	         tx = session.beginTransaction();
-	         List employees = session.createQuery("FROM Employee").list(); 
-	         for (Iterator iterator = 
-	                           employees.iterator(); iterator.hasNext();){
-	            App employee = (App) iterator.next(); 
-	            System.out.print("First Name: " + employee.getFname()); 
-	            System.out.print("  Last Name: " + employee.getLname()); 
-	            System.out.println("  Address: " + employee.getAddress()); 
-	            System.out.println("  Address: " + employee.getAddress()); 
-	         }
-	         tx.commit();
-	      }catch (HibernateException e) {
-	         if (tx!=null) tx.rollback();
-	         e.printStackTrace(); 
-	      }finally {
-	         session.close(); 
-	      }
-	   }
+
+	/* Method to READ all the employees */
+	public void listEmployees() {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			List employees = session.createQuery("FROM Employee").list();
+			for (Iterator iterator = employees.iterator(); iterator.hasNext();) {
+				App employee = (App) iterator.next();
+				System.out.print("First Name: " + employee.getFname());
+				System.out.print("  Last Name: " + employee.getLname());
+				System.out.println("  Address: " + employee.getAddress());
+				System.out.println("  Address: " + employee.getAddress());
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
 
 }
